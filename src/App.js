6fefa8a11bf0816502df088a9971037e10e605b2
mun <quangnd.edu@gmail.com>
2019-05-22 10:16:31 +0700

    Add basic components for Expense manager

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import './App.css';
-import './playground/redux101.js';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Header from "./Header";
+import Dashboard from "./Dashboard";
+import AddExpensePage from "./AddExpensePage";
+import EditExpensePage from "./EditExpensePage";
+import HelpPage from "./HelpPage";
+import NotFoundPage from "./NotFoundPage";
+import "./playground/redux101.js";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Expense manager</h1>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Dashboard} />
+          <Route path="/create" component={AddExpensePage} />
+          <Route path="/edit/:id" component={EditExpensePage} />
+          <Route path="/help" component={HelpPage} />
+          <Route component={NotFoundPage} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
