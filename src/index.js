25a4967f006ccdec4de018697fcc7cecc205eaa1
mun <quangnd.edu@gmail.com>
2019-05-23 18:28:28 +0700

    Mocking Libraries with Jest

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import "react-dates/lib/css/_datepicker.css";
 import { Provider } from "react-redux";
 import App from "./App";
 import configureStore from "./store/configureStore";
